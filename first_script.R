73b800eba24b1c5721916801299d771b4aef02d6
Sarthak Khadka <sarthakkhadka1@gmail.com>
2021-06-04 16:14:01 -0500

    Final Fifth Commit

--- first_script.R
+++ first_script.R
@@ -14,4 +14,29 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
 
-sxcx
+#Encoding Categorical Data
+
+dataset$Country = factor(dataset$Country,
+                         levels = c("France", "Spain", "Germany"),
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c("No", "Yes"),
+                           labels = c(0,1))
+
+#Splitting training and testing set from dataset (USing package caTools)
+
+#install.packages('caTools')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+#Feature Scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
+
